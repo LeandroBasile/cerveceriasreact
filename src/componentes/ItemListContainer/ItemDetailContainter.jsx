4c83458577f298f2fca75a652bde3712b466d97d
Leandro Basile <alesiobasile@gmail.com>
2022-01-31 21:06:22 -0300

    Firebase2-basile

--- src/componentes/ItemListContainer/ItemDetailContainter.jsx
+++ src/componentes/ItemListContainer/ItemDetailContainter.jsx
@@ -7,20 +7,36 @@ import { doc, getDoc, getFirestore } from "firebase/firestore";
 export const ItemDetailContainter = () => {
   const [producto, setProducto] = useState({});
   const { id } = useParams();
-  
+  const [cargando, setCargando] = useState(true)
+
   useEffect(() => {
     const db = getFirestore();
 
+    if(id){
     const queryProducto = doc(db, "items", id);
-    getDoc(queryProducto).then((resp) => {
-      setProducto({ id: resp.id, ...resp.data() });
-    });
+    getDoc(queryProducto)
+      .then((resp) => {
+        setProducto({ id: resp.id, ...resp.data() });
+      })
+      .catch((err) => console.log(err))
+      .finally(() => setCargando(false));
+    }else{
+      setCargando(true)
+    }
+      
   }, [id]);
 
   console.log(producto);
   return (
     <div>
-      <ItemDetail producto={producto} />
+      {cargando ? (
+        <div className="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      ) : (
+        <ItemDetail producto={producto} />
+      )}
+      
     </div>
   );
 };
